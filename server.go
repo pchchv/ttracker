9d2995700613cb90b8eb03a5d8255a941d4b0e30
pchchv <ipchchv@gmail.com>
2023-04-24 13:35:28 +0300

    Implement a createCalendarHandler handler

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -14,10 +15,22 @@ func pingHandler(c echo.Context) error {
 	return c.String(http.StatusOK, message)
 }
 
+func createCalendarHandler(c echo.Context) error {
+	year, err := strconv.Atoi(c.QueryParam("year"))
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Error when parsing the year")
+	}
+
+	calendar := newCalendar(year)
+
+	return c.JSON(http.StatusOK, calendar)
+}
+
 // The declaration of all routes comes from it.
 func routes(e *echo.Echo) {
 	e.GET("/", pingHandler)
 	e.GET("/ping", pingHandler)
+	e.POST("/newyear", createCalendarHandler)
 }
 
 func server() {
