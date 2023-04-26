13d6da7d1da6ef1089b73911ed127d25780e2319
pchchv <ipchchv@gmail.com>
2023-04-26 15:31:41 +0300

    Refactoring. Remove calendar

--- main.go
+++ main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strconv"
 
 	"github.com/pchchv/env"
 	"github.com/pchchv/golog"
 )
-
-type Day struct {
-	Month   string
-	Date    int16
-	Weekend bool
-	Holiday bool
-	Workday bool
-}
-
-type Calendar struct {
-	Year int16
-	Days []Day
-}
-
-type Worker struct {
-	Name             string
-	Department       string
-	PersonalCalendar Calendar
-}
 
 var testURL string
 
@@ -44,60 +23,6 @@ func getEnvValue(v string) string {
 		golog.Panic("Value %v does not exist", v)
 	}
 	return value
-}
-
-func newCalendar(jsonMap map[string]interface{}) (c Calendar, err error) {
-	_ = fmt.Sprint(jsonMap["Leap"]) // TODO: Convert to bool
-	year, err := strconv.Atoi(fmt.Sprint(jsonMap["Year"]))
-	if err != nil {
-		return
-	}
-
-	c.Year = int16(year)
-	c.Days = newYear(true) // leap
-
-	return
-}
-
-func newYear(leap bool) []Day {
-	var days []Day
-	var numDays int
-
-	if leap {
-		numDays = 366
-	} else {
-		numDays = 365
-	}
-
-	for i := 1; i <= numDays; i++ {
-		days = append(days, newDay())
-	}
-
-	return days
-}
-
-func newDay() (d Day) {
-	// TODO
-	return
-}
-
-func newWorker(name string, dept string, year Calendar) (w Worker) {
-	w.Name = name
-	w.Department = dept
-	w.PersonalCalendar = year
-
-	return
-}
-
-func createPerson(jsonMap map[string]interface{}) Worker {
-	name := fmt.Sprint(jsonMap["Name"])
-	department := fmt.Sprint(jsonMap["Department"])
-
-	// TODO: Implement the receipt of the calendar.
-	// Creating a new one is a bad idea.
-	calendar := Calendar{}
-
-	return newWorker(name, department, calendar)
 }
 
 func main() {
--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -15,35 +14,10 @@ func pingHandler(c echo.Context) error {
 	return c.String(http.StatusOK, message)
 }
 
-func createCalendarHandler(c echo.Context) error {
-	var jsonMap map[string]interface{}
-	if err := c.Bind(&jsonMap); err != nil {
-		return c.String(http.StatusBadRequest, fmt.Sprintf("Some input error: %e", err))
-	}
-
-	calendar, err := newCalendar(jsonMap)
-	if err != nil {
-		return c.String(http.StatusBadRequest, fmt.Sprintf("Some input error: %e", err))
-	}
-
-	return c.JSON(http.StatusOK, calendar)
-}
-
-func createPersonHandler(c echo.Context) error {
-	var jsonMap map[string]interface{}
-	if err := c.Bind(&jsonMap); err != nil {
-		return c.String(http.StatusBadRequest, fmt.Sprintf("Some input error: %e", err))
-	}
-
-	person := createPerson(jsonMap)
-	return c.JSON(http.StatusOK, person)
-}
-
 // The declaration of all routes comes from it.
 func routes(e *echo.Echo) {
 	e.GET("/", pingHandler)
 	e.GET("/ping", pingHandler)
-	e.POST("/newyear", createCalendarHandler)
 }
 
 func server() {
